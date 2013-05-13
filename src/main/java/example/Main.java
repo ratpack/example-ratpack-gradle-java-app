ef948a08171b112bed54605f958c1a91dde635f1
Luke Daley <ld@ldaley.com>
2013-05-14 00:49:01 +0100

    Use the public API.

--- src/main/java/example/Main.java
+++ src/main/java/example/Main.java
@@ -3,7 +3,7 @@ package example;
 import org.ratpackframework.bootstrap.RatpackServer;
 import org.ratpackframework.bootstrap.RatpackServerBuilder;
 import org.ratpackframework.guice.GuiceBackedHandlerFactory;
-import org.ratpackframework.guice.internal.DefaultGuiceBackedHandlerFactory;
+import org.ratpackframework.guice.Injection;
 import org.ratpackframework.routing.Handler;
 
 import java.io.File;
@@ -13,7 +13,7 @@ import static org.ratpackframework.routing.Handlers.routes;
 public class Main {
 
     public static void main(String[] args) {
-        GuiceBackedHandlerFactory guiceHandler = new DefaultGuiceBackedHandlerFactory();
+        GuiceBackedHandlerFactory guiceHandler = Injection.handlerFactory();
         Handler handler = guiceHandler.create(new ModuleBootstrap(), routes(new RoutingBootstrap()));
         File baseDir = new File(System.getProperty("user.dir"));
         RatpackServerBuilder ratpackServerBuilder = new RatpackServerBuilder(handler, baseDir);
