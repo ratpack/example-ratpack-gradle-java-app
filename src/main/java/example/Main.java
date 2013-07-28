c50fc80066672a91497dae4348b71977f7d45d90
Luke Daley <ld@ldaley.com>
2013-07-28 17:05:56 +0100

    Update after code changes.

--- src/main/java/example/Main.java
+++ src/main/java/example/Main.java
@@ -1,8 +1,10 @@
 package example;
 
-import org.ratpackframework.bootstrap.RatpackServer;
-import org.ratpackframework.bootstrap.RatpackServerBuilder;
+import org.ratpackframework.server.DefaultRatpackServerSettings;
+import org.ratpackframework.server.RatpackServer;
+import org.ratpackframework.server.RatpackServerBuilder;
 import org.ratpackframework.handling.Handler;
+import org.ratpackframework.server.RatpackServerSettings;
 
 import java.io.File;
 
@@ -12,6 +14,14 @@ import static org.ratpackframework.handling.Handlers.chain;
 public class Main {
 
     public static void main(String[] args) throws Exception {
+        // The root of all file system paths in the application (will be src/ratpack)
+        File baseDir = new File(System.getProperty("user.dir"));
+
+        // Is the app in dev mode? Where code/templates can change?
+        boolean reloadable = true;
+
+        RatpackServerSettings serverSettings = new DefaultRatpackServerSettings(baseDir, reloadable);
+
         // Defines the Guice modules for our application
         ModuleBootstrap modulesConfigurer = new ModuleBootstrap();
 
@@ -19,13 +29,10 @@ public class Main {
         Handler appHandler = chain(new HandlerBootstrap());
 
         // A Handler that makes objects bound to Guice by modules available downstream
-        Handler guiceHandler = handler(modulesConfigurer, appHandler);
-
-        // The root of all file system paths in the application (will be src/ratpack)
-        File baseDir = new File(System.getProperty("user.dir"));
+        Handler guiceHandler = handler(serverSettings, modulesConfigurer, appHandler);
 
         // The server builder allows configuring the port etc.
-        RatpackServerBuilder ratpackServerBuilder = new RatpackServerBuilder(guiceHandler, baseDir);
+        RatpackServerBuilder ratpackServerBuilder = new RatpackServerBuilder(serverSettings, guiceHandler);
 
         // Start the server and block
         RatpackServer ratpackServer = ratpackServerBuilder.build();
