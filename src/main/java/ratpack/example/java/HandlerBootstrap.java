030f8c708c50ea80bb8bdfadc19eecf2e26a3900
Shaun Mangelsdorf <s.mangelsdorf@gmail.com>
2014-04-26 16:30:20 +1000

    Use Java 8 lambda notation for adding handlers

--- src/main/java/ratpack/example/java/HandlerBootstrap.java
+++ src/main/java/ratpack/example/java/HandlerBootstrap.java
@@ -28,50 +28,32 @@ public class HandlerBootstrap implements Action<Chain> {
         handlers
 
                 // Map to /foo
-                .handler("foo", new Handler() {
-                    public void handle(Context context) {
-                        context.render("from the foo handler");
-                    }
-                })
+                .handler("foo", context -> context.render("from the foo handler"))
 
-                        // Map to /bar
-                .handler("bar", new Handler() {
-                    public void handle(Context context) {
-                        context.render("from the bar handler");
-                    }
-                })
+                // Map to /bar
+                .handler("bar", context -> context.render("from the bar handler"))
 
-                        // Set up a nested routing block, where
-                .prefix("nested", new Action<Chain>() {
-                    public void execute(Chain nested) {
-                        // Map to /nested/*/*
-                        nested.handler(":var1/:var2?", new Handler() {
-                            public void handle(Context context) {
-                                // The path tokens are the :var1 and :var2 path components above
-                                Map<String, String> pathTokens = context.getPathTokens();
-                                context.render("from the nested handler, var1: " + pathTokens.get("var1") + ", var2: " + pathTokens.get("var2"));
-                            }
-                        });
-                    }
-                })
+                // Set up a nested routing block, where
+                .prefix("nested", this::nestedHandlers)
 
-                        // Map a dependency injected handler to
+                // Map a dependency injected handler to
                 .handler("injected", handlers.getRegistry().get(MyHandler.class))
 
-                        // Bind the /static app path to the src/ratpack/assets/images dir
-                        // Try /static/logo.png
-                .prefix("static", new Action<Chain>() {
-                    public void execute(Chain handlers) {
-                        handlers.assets("assets/images");
-                    }
-                })
+                // Bind the /static app path to the src/ratpack/assets/images dir
+                // Try /static/logo.png
+                .prefix("static", (Chain nested) -> nested.assets("assets/images"))
 
-                        // If nothing above matched, we'll get to here.
-                .handler(new Handler() {
-                    public void handle(Context context) {
-                        context.render("root handler!");
-                    }
-                });
+                // If nothing above matched, we'll get to here.
+                .handler(context -> context.render("root handler!"));
+    }
+
+    private void nestedHandlers(Chain nested) {
+        // Map to /nested/*/*
+        nested.handler(":var1/:var2?", context -> {
+            // The path tokens are the :var1 and :var2 path components above
+            Map<String, String> pathTokens = context.getPathTokens();
+            context.render("from the nested handler, var1: " + pathTokens.get("var1") + ", var2: " + pathTokens.get("var2"));
+        });
     }
 
 }
