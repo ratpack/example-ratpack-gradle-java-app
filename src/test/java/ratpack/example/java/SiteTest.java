5c0d72c23b1cca61f88e4ce5132937c9147a0a11
Shaun Mangelsdorf <s.mangelsdorf@gmail.com>
2014-04-27 05:07:16 +1000

    Remove unused import

--- src/test/java/ratpack/example/java/SiteTest.java
+++ src/test/java/ratpack/example/java/SiteTest.java
@@ -7,7 +7,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.net.URI;
-import org.junit.Ignore;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 import org.junit.Test;
