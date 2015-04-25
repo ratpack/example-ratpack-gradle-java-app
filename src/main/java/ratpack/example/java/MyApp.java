2aa92cd399067a6498d9d47f3b58262335e3330b
Luke Daley <ld@ldaley.com>
2015-04-25 15:43:17 +1000

    Update to 0.9.16.

--- src/main/java/ratpack/example/java/MyApp.java
+++ src/main/java/ratpack/example/java/MyApp.java
@@ -14,16 +14,16 @@ public class MyApp {
 
   public static void main(String[] args) {
     try {
-      RatpackServer.of(b -> b
-          .serverConfig(ServerConfig.findBaseDirProps())
-          .registry(Guice.registry(bindingSpec -> bindingSpec.add(new MyModule())))
+      RatpackServer.start(s -> s
+          .serverConfig(ServerConfig.findBaseDir())
+          .registry(Guice.registry(b -> b.add(new MyModule())))
           .handlers(chain -> chain
-              .handler("foo", context -> context.render("from the foo handler")) // Map to /foo
-              .handler("bar", context -> context.render("from the bar handler")) // Map to /bar
+              .handler("foo", ctx -> ctx.render("from the foo handler")) // Map to /foo
+              .handler("bar", ctx -> ctx.render("from the bar handler")) // Map to /bar
               .prefix("nested", nested -> { // Set up a nested routing block, which is delegated to `nestedHandler`
-                nested.handler(":var1/:var2?", context -> { // The path tokens are the :var1 and :var2 path components above
-                  Map<String, String> pathTokens = context.getPathTokens();
-                  context.render(
+                nested.handler(":var1/:var2?", ctx -> { // The path tokens are the :var1 and :var2 path components above
+                  Map<String, String> pathTokens = ctx.getPathTokens();
+                  ctx.render(
                       "from the nested handler, var1: " + pathTokens.get("var1") +
                           ", var2: " + pathTokens.get("var2")
                   );
@@ -31,7 +31,9 @@ public class MyApp {
               })
               .handler("injected", chain.getRegistry().get(MyHandler.class)) // Map to a dependency injected handler
               .prefix("static", nested -> nested.assets("assets/images")) // Bind the /static app path to the src/ratpack/assets/images dir
-              .handler(context -> context.render("root handler!")))).start();
+              .handler(ctx -> ctx.render("root handler!"))
+          )
+      );
     } catch (Exception e) {
       LOGGER.error("", e);
       System.exit(1);
--- src/test/java/ratpack/example/java/SiteTest.java
+++ src/test/java/ratpack/example/java/SiteTest.java
@@ -1,75 +1,60 @@
 package ratpack.example.java;
 
-import com.google.common.io.CharStreams;
 import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
-import ratpack.test.ApplicationUnderTest;
 import ratpack.test.MainClassApplicationUnderTest;
-
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.net.URI;
+import ratpack.test.http.TestHttpClient;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
 
 @RunWith(JUnit4.class)
 public class SiteTest {
 
-    String lineSeparator = System.getProperty("line.separator");
+  String lineSeparator = System.getProperty("line.separator");
 
-    MainClassApplicationUnderTest aut = new MainClassApplicationUnderTest(MyApp.class);
+  MainClassApplicationUnderTest aut = new MainClassApplicationUnderTest(MyApp.class);
 
-    @After
-    public void tearDown() {
-      aut.close();
-    }
+  @After
+  public void tearDown() {
+    aut.close();
+  }
 
-    @Test
-    public void fooHandler() {
-        assertEquals("from the foo handler", get("/foo"));
-    }
+  @Test
+  public void fooHandler() {
+    assertEquals("from the foo handler", get("foo"));
+  }
 
-    @Test
-    public void barHandler() {
-        assertEquals("from the bar handler", get("/bar"));
-    }
+  @Test
+  public void barHandler() {
+    assertEquals("from the bar handler", get("bar"));
+  }
 
-    @Test
-    public void nestedHandler() {
-        assertEquals("from the nested handler, var1: x, var2: null", get("/nested/x"));
-        assertEquals("from the nested handler, var1: x, var2: y", get("/nested/x/y"));
-    }
+  @Test
+  public void nestedHandler() {
+    assertEquals("from the nested handler, var1: x, var2: null", get("nested/x"));
+    assertEquals("from the nested handler, var1: x, var2: y", get("nested/x/y"));
+  }
 
-    @Test
-    public void injectedHandler() {
-        assertEquals("service value: service-value", get("/injected"));
-    }
+  @Test
+  public void injectedHandler() {
+    assertEquals("service value: service-value", get("injected"));
+  }
 
-    @Test
-    public void staticHandler() {
-        assertEquals("text asset" + lineSeparator, get("/static/test.txt"));
-    }
+  @Test
+  public void staticHandler() {
+    assertEquals("text asset" + lineSeparator, get("static/test.txt"));
+  }
 
-    @Test
-    public void rootHandler() {
-        assertEquals("root handler!", get("/"));
-        assertEquals("root handler!", get("/unknown-path"));
-    }
+  @Test
+  public void rootHandler() {
+    assertEquals("root handler!", get(""));
+    assertEquals("root handler!", get("unknown-path"));
+  }
 
-    private String get(String path) {
-        URI uri = aut.getAddress().resolve(path);
-        try(InputStream in = uri.toURL().openStream()) {
-            try (Reader r = new InputStreamReader(in)) {
-                return CharStreams.toString(r);
-            }
-        } catch (Exception ex) {
-            fail(ex.toString());
-            return ""; // unreachable
-        }
-    }
+  private String get(String path) {
+    return TestHttpClient.testHttpClient(aut).getText(path);
+  }
 
 }
