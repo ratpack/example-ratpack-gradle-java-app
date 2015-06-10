f9edaefae0949e1c3b41c94938b45825764d0559
Jeff Beck <beckje01@gmail.com>
2015-06-10 00:24:16 -0500

    Fix up example for the chain changes with 9.18

--- src/main/java/ratpack/example/java/MyApp.java
+++ src/main/java/ratpack/example/java/MyApp.java
@@ -3,6 +3,7 @@ package ratpack.example.java;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ratpack.guice.Guice;
+import ratpack.handling.Chain;
 import ratpack.server.RatpackServer;
 import ratpack.server.ServerConfig;
 
@@ -18,10 +19,10 @@ public class MyApp {
           .serverConfig(ServerConfig.findBaseDir())
           .registry(Guice.registry(b -> b.module(MyModule.class)))
           .handlers(chain -> chain
-              .handler("foo", ctx -> ctx.render("from the foo handler")) // Map to /foo
-              .handler("bar", ctx -> ctx.render("from the bar handler")) // Map to /bar
+              .path("foo", ctx -> ctx.render("from the foo handler")) // Map to /foo
+              .path("bar", ctx -> ctx.render("from the bar handler")) // Map to /bar
               .prefix("nested", nested -> { // Set up a nested routing block, which is delegated to `nestedHandler`
-                nested.handler(":var1/:var2?", ctx -> { // The path tokens are the :var1 and :var2 path components above
+                nested.path(":var1/:var2?", ctx -> { // The path tokens are the :var1 and :var2 path components above
                   Map<String, String> pathTokens = ctx.getPathTokens();
                   ctx.render(
                       "from the nested handler, var1: " + pathTokens.get("var1") +
@@ -29,9 +30,9 @@ public class MyApp {
                   );
                 });
               })
-              .handler("injected", chain.getRegistry().get(MyHandler.class)) // Map to a dependency injected handler
-              .prefix("static", nested -> nested.assets("assets/images")) // Bind the /static app path to the src/ratpack/assets/images dir
-              .handler(ctx -> ctx.render("root handler!"))
+              .path("injected", chain.getRegistry().get(MyHandler.class)) // Map to a dependency injected handler
+              .prefix("static", nested -> nested.fileSystem("assets/images", Chain::files)) // Bind the /static app path to the src/ratpack/assets/images dir
+              .all(ctx -> ctx.render("root handler!"))
           )
       );
     } catch (Exception e) {
